da0de63e32a43c6395a37e00b76e2e4d07a58546
TENET <xSomoy@protonmail.com>
2024-12-25 12:31:30 +0600

    happy christmas

--- happyChristmas.py
+++ happyChristmas.py
@@ -1,5 +1,5 @@
-# Happy Christmas 2024
-cht = "Happy Christmas 2024"
+# Happy Christmas
+cht = "Happy Christmas"
 print(cht)
 for leaf in [*range(10)] + [2]:
     print(f'{"x" * (leaf * 2 + 1):^30}')
